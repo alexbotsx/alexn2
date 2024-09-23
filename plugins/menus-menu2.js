e2cbe62e4b170f4a60f72369e4c5024e588df6e8
Xnnv2 <toscren0@gmail.com>
2024-09-22 20:50:51 -0500

    dx

--- plugins/menus-menu2.js
+++ plugins/menus-menu2.js
@@ -25,7 +25,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `ＭＥＮＵ | ＣＯＭＰＬＥＴＯ ⌟*
+    const str = `> *ＭＥＮＵ | ＣＯＭＰＬＥＴＯ ⌟*
 
 *╭━〔 𝐆𝐇𝐎𝐒𝐓 𝐁𝐎𝐓 〕━╮*
 ┃➔ ⭐ *Owner:* Alex
